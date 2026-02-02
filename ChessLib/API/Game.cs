e1c7eb2d63546a867b9b7de3047ca8e637e215fe
szalai.mate <szalai.mate@students.jedlik.eu>
2026-02-02 09:11:11 +0100

    game nodes

---
+++ ChessLib/API/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using ChessLib.Base;
+
+namespace ChessLib.API;
+
+public class Game(Board starting) : IEnumerable<Node>
+{
+    private readonly List<Node> _moves = new();
+    private readonly Board _current = starting;
+    
+    public Node this[int i] => _moves[i];
+    public Node this[Index i] => _moves[i];
+
+    public void MakeMove(Move move)
+    {
+        _moves.Add(new Node(_current.Clone(), move));
+        _current.MakeMove(move);
+    }
+    
+    public IEnumerator GetEnumerator()
+    {
+        return _moves.GetEnumerator();
+    }
+
+    IEnumerator<Node> IEnumerable<Node>.GetEnumerator()
+    {
+        return _moves.GetEnumerator();
+    }
+}
---
+++ ChessLib/API/Node.cs
@@ -0,0 +1,9 @@
+﻿using ChessLib.Base;
+
+namespace ChessLib.API;
+
+public struct Node(Board board, Move move)
+{
+    public Board Board = board;
+    public Move Move = move;
+}
