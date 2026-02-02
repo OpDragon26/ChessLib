fded2b58ab9c5b60bc04700572ca60089fc3c8e3
szalai.mate <szalai.mate@students.jedlik.eu>
2026-02-02 10:18:57 +0100

    .

---
+++ ChessLib/API/Parsing/FEN.cs
@@ -0,0 +1,11 @@
+﻿using ChessLib.Base;
+
+namespace ChessLib.API.Parsing;
+
+public static class FEN
+{
+    public static Board ParseFEN(string fen)
+    {
+        throw new NotImplementedException();
+    }
+}
