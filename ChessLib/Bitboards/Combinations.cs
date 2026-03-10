b5654f4c6a7a16ca553476786c3570a530b10dff
szalai.mate <szalai.mate@students.jedlik.eu>
2026-03-10 13:24:28 +0100

    bit combinations

---
+++ ChessLib/Bitboards/Combinations.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+
+namespace ChessLib.Bitboards;
+
+public static class Combinations
+{
+    private static bool Initialized;
+    
+    public static readonly ulong[][] Rook = new ulong[64][];
+    public static readonly ulong[][] Bishop = new ulong[64][];
+    public static readonly ulong[][] Knight = new ulong[64][];
+    public static readonly ulong[][] Queen = new ulong[64][];
+    public static readonly ulong[][] King = new ulong[64][];
+
+    public static void Init()
+    {
+        if (Initialized) 
+            return;
+        Initialized = true;
+    }
+
+    public static ulong[] GetCombinations(ulong mask)
+    {
+        int k = BitOperations.PopCount(mask);
+        ulong count = 1UL << k;
+        ulong[] result = new ulong[count];
+        
+        for (ulong i = 0; i < count; i++)
+            result[i] = Bmi2.X64.ParallelBitDeposit(i, mask);
+        return result;
+    }
+}
