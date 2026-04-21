31261fb315d7d1f7e8cc45b555d3b9fdb7c59e95
szalai.mate <szalai.mate@students.jedlik.eu>
2026-04-21 13:22:24 +0200

    started pins

---
+++ ChessLib/Magic_Lookup/Pins.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessLib.Magic_Lookup;
+
+public static class Pins
+{
+    public static Dictionary<int, int>[][] Pin = new Dictionary<int, int>[64][];
+
+    public static Dictionary<int, int>[] FindPotentialPins(int square, ulong combination)
+    {
+        
+    }
+}
